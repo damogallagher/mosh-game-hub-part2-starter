b0bdd52e90791eed8d653c3dd5c7ae9b61db03c9
Damien Gallagher <damien.gallagher@gmail.com>
2024-04-02 22:20:00 +0100

    Update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "554a517b8f294a5d9f85713195e71a67",
   },
 });
